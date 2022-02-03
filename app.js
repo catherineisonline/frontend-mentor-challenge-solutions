83d169adbb34539d83382dcfc2aef4a8b904db8e
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-02-03 20:51:32 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,117 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    solutions: [
+      {
+        id: 1,
+        projectName: "Stats preview card component",
+        projectDescribtion:
+          "This is a great small challenge to help get you used to building to a design. There's no JS in this project, so you'll be able to focus on your HTML & CSS skills.",
+        projectStack: ["HTML", "CSS"],
+        projectPreview: "images/stats-preview-card-component.jpg",
+        liveLink: "https://catherineisonline.github.io/stats-preview-card-component-frontendmentor/",
+        projectLink: "https://www.frontendmentor.io/challenges/stats-preview-card-component-8JqbgoU62",
+      },
+      {
+        id: 2,
+        projectName: "Order summary component",
+        projectDescribtion:
+          "A perfect project for newbies who are starting to build confidence with layouts!",
+        projectStack: ["HTML", "CSS"],
+        projectPreview: "images/order-summary-component.jpg",
+        liveLink: "https://catherineisonline.github.io/order-summary-component-frontendmentor/",
+      },
+      {
+        id: 3,
+        projectName: "Profile card component",
+        projectDescribtion:
+          "This is a perfect challenge to test your layout skills. The card layout doesn't shift, so it's also great for those that haven't dived into responsive websites yet!",
+        projectStack: ["HTML", "CSS"],
+        projectPreview: "images/profile-card-component.jpg",
+        liveLink: "https://catherineisonline.github.io/profile-card-component-frontendmentor/",
+      },
+      {
+        id: 4,
+        projectName: "Single price grid component",
+        projectDescribtion:
+          "In this challenge, you will build out the pricing component to the designs provided. This is perfect for beginners and people who want to complete a smaller challenge.",
+        projectStack: ["HTML", "CSS"],
+        projectPreview: "images/single-price-grid-component.jpg",
+        liveLink: "https://catherineisonline.github.io/single-price-grid-component-frontendmentor/",
+      },
+      {
+        id: 5,
+        projectName: "3-column preview card component",
+        projectDescribtion:
+          "This challenge is perfect if you're just getting started. The shift between the layouts will be a nice test if you're new to building responsive projects.",
+        projectStack: ["HTML", "CSS"],
+        projectPreview: "images/3-column-preview-card-component.jpg",
+        liveLink: "https://catherineisonline.github.io/3-column-card-component-frontendmentor/",
+      },
+      {
+        id: 6,
+        projectName: "Social proof section",
+        projectDescribtion:
+          "This project will test your layout skills. If you're starting to get confident with Flexbox or Grid, this will provide a nice challenge!",
+        projectStack: ["HTML", "CSS"],
+        projectPreview: "images/social-proof-section.jpg",
+        liveLink: "https://catherineisonline.github.io/social-proof-section-frontendmentor/",
+      },
+      {
+        id: 7,
+        projectName: "Four card feature section",
+        projectDescribtion:
+          "A nice layout-based challenge for beginners. This will test anyone who is new to multi-column and responsive layouts.",
+        projectStack: ["HTML", "CSS"],
+        projectPreview: "images/four-card-feature-section.jpg",
+        liveLink: "https://catherineisonline.github.io/four-card-feature-section-frontendmentor/",
+      },
+      {
+        id: 8,
+        projectName: "NFT preview card component",
+        projectDescribtion:
+          "This HTML & CSS only challenge is perfect for anyone just starting out or anyone wanting a small project to play around with.",
+        projectStack: ["HTML", "CSS"],
+        projectPreview: "images/NFT-preview-card-component.jpg",
+        liveLink: "https://catherineisonline.github.io/nft-preview-card-frontendmentor/",
+      },
+      {
+        id: 9,
+        projectName: "Huddle landing page with a single introductory section",
+        projectDescribtion:
+          "A perfect challenge for beginners, this project will get you working with a two column layout.",
+        projectStack: ["HTML", "CSS"],
+        projectPreview:
+          "images/huddle-landing-page-with-a-single-introductory section.jpg",
+        liveLink: "https://catherineisonline.github.io/huddle-landing-page-with-a-single-introductory-section-frontendmentor/",
+      },
+      {
+        id: 10,
+        projectName: "Fylo data storage component",
+        projectDescribtion:
+          "This component has some interesting CSS challenges in the design. If you're looking to test your CSS skills, this will be a great project for you!",
+        projectStack: ["HTML", "CSS"],
+        projectPreview: "images/fylo-data-storage-component.jpg",
+        liveLink: "https://catherineisonline.github.io/fylo-data-storage-component-frontendmentor/",
+      },
+      {
+        id: 11,
+        projectName: "QR code component",
+        projectDescribtion:
+          "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
+        projectStack: ["HTML", "CSS"],
+        projectPreview: "images/QR-code-component.jpg",
+        liveLink: "https://catherineisonline.github.io/QR-code-component-frontendmentor/",
+      },
+    ],
+  },
+
+  // methods: {
+  //   mouseover() {
+  //     document.querySelector(".image-hover").classList.add("shown");
+  //   },
+  //   mouseleave() {
+  //     document.querySelector(".image-hover").classList.remove("shown");
+  //   },
+  // },
+});
