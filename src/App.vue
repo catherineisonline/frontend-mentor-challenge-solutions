f08c38f7f77b943a7877255d7ec027fbe8b4cdcc
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-02-09 22:38:43 +0400

    changing to vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,242 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="all-solutions">
+    <h2 class="all-solutions-h2">All solutions</h2>
+    <!-- Solutions Container -->
+    <ul class="solutions">
+      <li
+        class="solution"
+        v-for="solution in solutions"
+        v-bind:key="solution.id"
+      >
+        <!-- Project Preview -->
+        <div class="project-preview">
+          <img
+            class="project-image"
+            v-bind:src="solution.projectPreview"
+            alt="project preview"
+          />
+        </div>
+        <!-- Project Info -->
+        <div class="project-info">
+          <!-- Project Name -->
+          <div class="project-name">
+            <p>{{ solution.projectName }}</p>
+          </div>
+          <!-- Project Stack -->
+          <div class="project-stacks">
+            <p
+              class="project-stack"
+              v-for="projectStack in solution.projectStack"
+              v-bind:class="projectStack"
+              v-bind:key="projectStack"
+            >
+              {{ projectStack }}
+            </p>
+          </div>
+          <!-- Project Describtion -->
+          <div class="project-describtion">
+            <p>{{ solution.projectDescribtion }}</p>
+          </div>
+          <!-- Links -->
+          <!-- <div
+            class="links-container"
+            @mouseenter="mouseenter"
+            @mouseleave="mouseleave"
+          > -->
+          <div class="links-container">
+            <button class="learn-more-btn">Learn more</button>
+            <ul class="links">
+              <li class="link-btn">
+                <a v-bind:href="solution.liveLink" target="_blank">Live View</a>
+              </li>
+              <li class="link-btn">
+                <a v-bind:href="solution.repoLink" target="_blank">Repo</a>
+              </li>
+              <li class="link-btn">
+                <a v-bind:href="solution.projectLink" target="_blank"
+                  >Try Out</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </section>
+  <!-- <HelloWorld></HelloWorld> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      solutions: [
+        {
+          id: 1,
+          projectName: "Stats preview card component",
+          projectDescribtion:
+            "This is a great small challenge to help get you used to building to a design. There's no JS in this project, so you'll be able to focus on your HTML & CSS skills.",
+          projectStack: ["HTML", "CSS"],
+          projectPreview: require("@/assets/images/stats-preview-card-component.jpg"),
+          liveLink:
+            "https://catherineisonline.github.io/stats-preview-card-component-frontendmentor/",
+          projectLink:
+            "https://www.frontendmentor.io/challenges/stats-preview-card-component-8JqbgoU62",
+          repoLink:
+            "https://github.com/catherineisonline/stats-preview-card-component-frontendmentor",
+        },
+        {
+          id: 2,
+          projectName: "Order summary component",
+          projectDescribtion:
+            "A perfect project for newbies who are starting to build confidence with layouts!",
+          projectStack: ["HTML", "CSS"],
+          projectPreview: require("@/assets/images/order-summary-component.jpg"),
+          liveLink:
+            "https://catherineisonline.github.io/order-summary-component-frontendmentor/",
+          projectLink:
+            "https://www.frontendmentor.io/challenges/order-summary-component-QlPmajDUj",
+          repoLink:
+            "https://github.com/catherineisonline/order-summary-component-frontendmentor",
+        },
+        {
+          id: 3,
+          projectName: "Profile card component",
+          projectDescribtion:
+            "This is a perfect challenge to test your layout skills. The card layout doesn't shift, so it's also great for those that haven't dived into responsive websites yet!",
+          projectStack: ["HTML", "CSS"],
+          projectPreview: require("@/assets/images/profile-card-component.jpg"),
+          liveLink:
+            "https://catherineisonline.github.io/profile-card-component-frontendmentor/",
+          projectLink:
+            "https://www.frontendmentor.io/challenges/profile-card-component-cfArpWshJ",
+          repoLink:
+            "https://github.com/catherineisonline/profile-card-component-frontendmentor",
+        },
+        {
+          id: 4,
+          projectName: "Single price grid component",
+          projectDescribtion:
+            "In this challenge, you will build out the pricing component to the designs provided. This is perfect for beginners and people who want to complete a smaller challenge.",
+          projectStack: ["HTML", "CSS"],
+          projectPreview: require("@/assets/images/single-price-grid-component.jpg"),
+          liveLink:
+            "https://catherineisonline.github.io/single-price-grid-component-frontendmentor/",
+
+          projectLink:
+            "https://www.frontendmentor.io/challenges/single-price-grid-component-5ce41129d0ff452fec5abbbc",
+          repoLink:
+            "https://github.com/catherineisonline/single-price-grid-component-frontendmentor",
+        },
+        {
+          id: 5,
+          projectName: "3-column preview card component",
+          projectDescribtion:
+            "This challenge is perfect if you're just getting started. The shift between the layouts will be a nice test if you're new to building responsive projects.",
+          projectStack: ["HTML", "CSS"],
+          projectPreview: require("@/assets/images/3-column-preview-card-component.jpg"),
+          liveLink:
+            "https://catherineisonline.github.io/3-column-card-component-frontendmentor/",
+          projectLink:
+            "https://www.frontendmentor.io/challenges/3column-preview-card-component-pH92eAR2-",
+          repoLink:
+            "https://github.com/catherineisonline/3-column-card-component-frontendmentor",
+        },
+        {
+          id: 6,
+          projectName: "Social proof section",
+          projectDescribtion:
+            "This project will test your layout skills. If you're starting to get confident with Flexbox or Grid, this will provide a nice challenge!",
+          projectStack: ["HTML", "CSS"],
+          projectPreview: require("@/assets/images/social-proof-section.jpg"),
+          liveLink:
+            "https://catherineisonline.github.io/social-proof-section-frontendmentor/",
+
+          projectLink:
+            "https://www.frontendmentor.io/challenges/social-proof-section-6e0qTv_bA",
+          repoLink:
+            "https://github.com/catherineisonline/social-proof-section-frontendmentor",
+        },
+        {
+          id: 7,
+          projectName: "Four card feature section",
+          projectDescribtion:
+            "A nice layout-based challenge for beginners. This will test anyone who is new to multi-column and responsive layouts.",
+          projectStack: ["HTML", "CSS"],
+          projectPreview: require("@/assets/images/four-card-feature-section.jpg"),
+          liveLink:
+            "https://catherineisonline.github.io/four-card-feature-section-frontendmentor/",
+
+          projectLink:
+            "https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK",
+          repoLink:
+            "https://github.com/catherineisonline/four-card-feature-section-frontendmentor",
+        },
+        {
+          id: 8,
+          projectName: "NFT preview card component",
+          projectDescribtion:
+            "This HTML & CSS only challenge is perfect for anyone just starting out or anyone wanting a small project to play around with.",
+          projectStack: ["HTML", "CSS"],
+          projectPreview: require("@/assets/images/NFT-preview-card-component.jpg"),
+          liveLink:
+            "https://catherineisonline.github.io/nft-preview-card-frontendmentor/",
+
+          projectLink:
+            "https://www.frontendmentor.io/challenges/nft-preview-card-component-SbdUL_w0U",
+          repoLink:
+            "https://github.com/catherineisonline/nft-preview-card-frontendmentor",
+        },
+        {
+          id: 9,
+          projectName: "Huddle landing page with a single introductory section",
+          projectDescribtion:
+            "A perfect challenge for beginners, this project will get you working with a two column layout.",
+          projectStack: ["HTML", "CSS"],
+          projectPreview: require("@/assets/images/huddle-landing-page-with-a-single-introductory section.jpg"),
+          liveLink:
+            "https://catherineisonline.github.io/huddle-landing-page-with-a-single-introductory-section-frontendmentor/",
+
+          projectLink:
+            "https://www.frontendmentor.io/challenges/huddle-landing-page-with-a-single-introductory-section-B_2Wvxgi0",
+          repoLink:
+            "https://github.com/catherineisonline/huddle-landing-page-with-a-single-introductory-section-frontendmentor",
+        },
+        {
+          id: 10,
+          projectName: "Fylo data storage component",
+          projectDescribtion:
+            "This component has some interesting CSS challenges in the design. If you're looking to test your CSS skills, this will be a great project for you!",
+          projectStack: ["HTML", "CSS"],
+          projectPreview: require("@/assets/images/fylo-data-storage-component.jpg"),
+          liveLink:
+            "https://catherineisonline.github.io/fylo-data-storage-component-frontendmentor/",
+          projectLink:
+            "https://www.frontendmentor.io/challenges/fylo-data-storage-component-1dZPRbV5n",
+          repoLink:
+            "https://github.com/catherineisonline/fylo-data-storage-component-frontendmentor",
+        },
+        {
+          id: 11,
+          projectName: "QR code component",
+          projectDescribtion:
+            "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
+          projectStack: ["HTML", "CSS"],
+          projectPreview: require("@/assets/images/QR-code-component.jpg"),
+          liveLink:
+            "https://catherineisonline.github.io/QR-code-component-frontendmentor/",
+
+          projectLink:
+            "https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H",
+          repoLink:
+            "https://github.com/catherineisonline/QR-code-component-frontendmentor",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './assets/reset.css';
+import './assets/styles.css';
+
 Vue.config.productionTip = false
 
 new Vue({
