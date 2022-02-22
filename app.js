d8aabcbd72f8b5b314123372187679b5161621e7
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-02-22 12:34:03 +0400

    udpated mobile menu

--- app.js
+++ app.js
@@ -97,14 +97,24 @@ const apiFunc = shortlyBtn.addEventListener("click", function () {
 //   cloneLinkField.innerHTML = cloneLink;
 // };
 
+
+
 //Hamburger Menu
-
 const burgerIcon = document.querySelector(".fa-bars");
-const navMenu = document.querySelector(" .menu-section");
+const navMenu = document.querySelector(".menu-section");
+const mainElement = document.querySelector(".main-container");
 
-const menuToggle = () =>   navMenu.classList.toggle("show");
+//Toggle Menu
+const menuToggle = () => navMenu.classList.toggle("show");
+//Remove Menu
+const removeMenu = () => navMenu.classList.remove("show");
 
+//Toggle menu with hamburger
 burgerIcon.addEventListener("click", function () {
-menuToggle();
+  menuToggle();
 });
 
+//Remove menu by clicking outside navigation
+mainElement.addEventListener("click", function () {
+  removeMenu();
+});
