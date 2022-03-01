5fd90cab55cbfbda73046817f2b16f5d9937f46e
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-03-01 12:07:06 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const inputEl = document.querySelector(".email-input");
+const submitBtn = document.querySelector(".submit-btn");
+const errorMsg = document.querySelector(".email-input-message");
+const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const timeoutFunc = setInterval(function () {
+  errorMsg.style.display = "none";
+}, 2500);
+
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (inputEl.value.length === 0) {
+    errorMsg.style.display = "inline";
+    errorMsg.textContent = "Whoops! It looks like you forgot to add your email";
+    timeoutFunc;
+  } else if (
+    !inputEl.value.match(emailValidation) &&
+    inputEl.value.length !== 0
+  ) {
+    errorMsg.style.display = "inline";
+    errorMsg.textContent = "Please provide a valid email address";
+    timeoutFunc;
+  } else {
+    errorMsg.style.display = "inline";
+    errorMsg.style.color = "green";
+    errorMsg.textContent = "We sent you a gift, check it out!";
+    timeoutFunc;
+  }
+});
