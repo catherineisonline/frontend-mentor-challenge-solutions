6bfddd68b2f63138d0face456751d900fe5d71c8
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-04-22 10:09:54 +0400

    fixinf deployment

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-import Vue from 'vue';
-import App from './App.vue';
-import './assets/styles.sass';
+import Vue from "vue";
+import App from "./App.vue";
+import "./assets/styles.sass";
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
-
+  render: (h) => h(App),
+}).$mount("#app");
