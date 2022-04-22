3950368561bba98360faedd4eab89c67b09a7db2
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-04-22 10:31:31 +0400

    fixing deploy

---
+++ vue.config.js
@@ -0,0 +1,5 @@
+// vue.config.js file to be placed in the root of your repository
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/frontend-mentor-challenge-solutions" : "/",
+};
