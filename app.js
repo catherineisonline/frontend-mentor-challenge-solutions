d563e309297c6c87da368c183a179314ade082c9
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-07-13 18:13:22 +0400

    fixed input message bug

--- app.js
+++ app.js
@@ -5,26 +5,22 @@ const submitBtn = document.querySelector(".submit-btn");
 const errorMsg = document.querySelector(".email-input-message");
 const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-const timeoutFunc = setInterval(() => {
-  errorMsg.style.display = "none";
-}, 2500);
 
 submitBtn.addEventListener("click", function () {
   if (inputEl.value.length === 0) {
     errorMsg.style.display = "inline";
     errorMsg.textContent = "Whoops! It looks like you forgot to add your email";
-    timeoutFunc;
+    errorMsg.style.color = "var(--light-red)";
   } else if (
     !inputEl.value.match(emailValidation) &&
     inputEl.value.length !== 0
   ) {
     errorMsg.style.display = "inline";
     errorMsg.textContent = "Please provide a valid email address";
-    timeoutFunc;
+    errorMsg.style.color = "var(--light-red)";
   } else {
     errorMsg.style.display = "inline";
     errorMsg.style.color = "green";
     errorMsg.textContent = "We sent you a gift, check it out!";
-    timeoutFunc;
   }
 });
