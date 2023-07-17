0b446163a35a89ba54c52e1b6b9bffbb92227e74
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-07-17 17:29:18 +0400

    refactored code

--- app.js
+++ app.js
@@ -5,19 +5,15 @@ const inputEl = document.querySelector(".input-field");
 
 const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-const timeoutFunc = setTimeout(() => {
-  errorMsg.style.display = "none";
-}, 3000);
 
 sendBtn.addEventListener("click", function () {
   if (inputEl.value.length === 0 || !inputEl.value.match(emailValidation)) {
-    errorMsg.style.display = "inline";
+    errorMsg.style.display = "inline-block";
+    errorMsg.style.color = "var(--error)";
     errorMsg.textContent = "Please enter a valid email address";
-    return timeoutFunc;
   } else {
-    errorMsg.style.display = "inline";
+    errorMsg.style.display = "inline-block";
     errorMsg.style.color = "green";
     errorMsg.textContent = "Thank you for subscribing to our newsletter!";
-    return timeoutFunc;
   }
 });
