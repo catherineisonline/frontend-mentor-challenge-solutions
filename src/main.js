e42c1d2add5def965a85803083c78e42230bbd17
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-07-25 10:48:25 +0400

    Initial dist subtree commit

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./assets/styles.sass";
-
-// Vue.config.productionTip = false;
-
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
