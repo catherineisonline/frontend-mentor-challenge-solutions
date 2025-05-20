05fd0c6a9656906ef4194dbc9ccb1aa96fb0a65a
catherineisonline <ekaterine.jpg@gmail.com>
2025-05-20 18:55:31 +0700

    improve name validation

--- src/helpers/validate.js
+++ src/helpers/validate.js
@@ -2,17 +2,20 @@ import { isPossiblePhoneNumber } from "react-phone-number-input";
 
 export const validate = (target, targetIndex) => {
   let errors = {};
-  const emailReg = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  const isEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  const hasNumber = /\d/;
   if (targetIndex === 0) {
     let { name, email, number } = target;
     if (name.length === 0) {
       errors.name = "This field is required";
     } else if (name.length < 3) {
       errors.name = "Name should be min. 3 characters";
+    } else if (hasNumber.test(name)) {
+      errors.name = "Name shouldn't contain numbers";
     }
     if (email.length === 0) {
       errors.email = "This field is required";
-    } else if (!emailReg.test(email)) {
+    } else if (!isEmail.test(email)) {
       errors.email = "Invalid email format";
     }
     if (number.length === 0) {
